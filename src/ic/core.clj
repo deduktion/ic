efe9363efc10ef9fb7b97f1a4a1de91e11e19894
thrstnh <thrstn.hllbrnd@googlemail.com>
2013-10-20 01:14:32 +0200

    seperate arguments

--- src/ic/core.clj
+++ src/ic/core.clj
@@ -13,19 +13,23 @@
 (set-logger! :pattern log-pattern)
 (info "ic start " (msec))
 
+(defn arguments
+  "specify arguments"
+  [args]
+  (cli args
+      ["-h" "--help" "show help"]
+      ["-r" "--rescan" "rescan store" :flag false]
+      ["-a" "--algorithm" "set cryptographic hash type" :default "sha-256"]
+      ["-s" "--store" "store"]
+        ["-n" "--new" "new store" :flag false]
+        ["-d" "--delete" "delete a store" :flag false]
+        ["-l" "--list" "list stores" :flag false]
+        ["-p" "--path" "path to a directory with your files to index"]))
+
 (defn -main
   "int-check in clojure"
   [& args]
-  (let [[options args banner] 
-      (cli args
-         ["-h" "--help" "show help"]
-         ["-r" "--rescan" "rescan store" :flag false]
-         ["-a" "--algorithm" "set cryptographic hash type" :default "sha-256"]
-         ["-s" "--store" "store"]
-           ["-n" "--new" "new store" :flag false]
-           ["-d" "--delete" "delete a store" :flag false]
-           ["-l" "--list" "list stores" :flag false]
-           ["-p" "--path" "path to a directory with your files to index"])]
+  (let [[options args banner] (arguments args)]
     (init-config)
     (with-connection db
       (if (contains? options :help) (println banner))
