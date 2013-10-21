0ca1f3cecff11a66a5abbf68b15380498f1614a5
thrstnh <thrstn.hllbrnd@googlemail.com>
2013-10-21 23:51:06 +0200

    more gui stuff

--- src/ic/ui/gui.clj
+++ src/ic/ui/gui.clj
@@ -1,20 +1,18 @@
 (ns ic.ui.gui
   (:use [ic tools stats]
-        [seesaw core]
+        [seesaw core tree]
         [clojure.tools.logging :only (info error)]
-        [clj-logging-config.log4j]))
+        [clj-logging-config.log4j])
+  (:import [java.io.File]))
 (set-logger! :pattern log-pattern)
+
+(def border-with 1)
 
 (def table-header (list
   :path :chksum :algorithm
   :size :took
   :r :w :x
   :hidden))
-
-(defn make-table [header data]
-  (table :id :file-table
-         :model [:columns header
-                 :rows data]))
 
 (defn table-data
   "create table data from entries"
@@ -31,34 +29,71 @@
       (map :x entries)
       (map :hidden entries)))
 
+(defn make-table [entries]
+  (scrollable
+    (table :id :file-table
+           :model [:columns table-header
+                   :rows (table-data entries)])))
+
+(defn file-exit [e] (System/exit 0))
+
+(def menus
+  (let [file-exit (action :handler file-exit
+                          :name "exit"
+                          :tip "exit ic")]
+    (menubar :items [(menu :text "file"
+                           :items [file-exit])
+                     (menu :text "help"
+                           :items [])])))
+  
+(def file-detail-panel
+  (text :id :file-details
+        :text "file details"
+        :multi-line? false
+        :editable? false))
+
+(def statusbar-panel
+  (text :id :statusbar-panel
+        :text "TODO: status bar"))
+
+(def tree-model
+  (simple-tree-model
+    #(.isDirectory %)
+    (fn [f] (filter #(.isDirectory %) (.listFiles f)))
+    (java.io.File. ".")))
+
+(def tree-panel
+  (scrollable
+    (tree :id :tree-view
+          :model tree-model)))
+
 (defn make-content [entries]
-  (border-panel :border 5
-                :hgap 5
-                :vgap 5
-      :north (label :id :current-dir
-                    :text "menu")
-      :west (label :id :file-tree
-                   :text "tree"
-                   :visible? false)
-      :center (scrollable
-                (make-table
-                  table-header
-                  (table-data entries)))
-      :east (label :id :file-props
-                   :text "file-props"
-                   :visible? false)
-      :south (label :id :status
-                    :text (str (total-stats)))))
+  (border-panel
+    :border border-with
+    :hgap border-with
+    :vgap border-with
+    :west tree-panel
+    :center (make-table entries)
+    :east file-detail-panel
+    :south statusbar-panel))
+
+(defn make-main-frame
+  "create the mail windows"
+  [entries]
+  (frame
+    :title "IC"
+    :menubar menus
+    :content (make-content entries)
+    :width 1200
+    :height 400
+    :minimum-size [640 :by 480]
+    :visible? true
+    :on-close :exit))
 
 (defn switch-to-gui
   "run in gui mode"
   [entries]
   (native!)
-  (let [f (frame :title "IC"
-                 :content (make-content entries)
-                 :width 1200
-                 :height 400
-                 :visible? true
-                 :on-close :exit)]
+  (let [f (make-main-frame entries)]
     (.setLocation f (java.awt.Point. 100 300))
     (timer (fn [e] (repaint! (select f [:#:clock])) 1000))))
