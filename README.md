d062b7a519c8f1daef41f643cb300cc76550063a
thrstnh <matflasch@y.fritz.box>
2013-10-21 03:48:19 +0200

    readme

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-ic
-==
+# ic
+### so far a basic first example
 
-none yet
+## set algorithm
+* lein run -a md5
+* lein run -a sha-1
+* lein run -a sha-256
+* lein run -a sha-512
+
+## set interval for next scan in seconds
+* lein run -c 30   ;;(rescan after 30 seconds)
+* lein run -c 2592000  ;;(rescan after 1 month)
+
+## add new store
+* lein run -s storename -p /path/to/store -n
+* lein run -s otherstore -p /path/to/other/store -n
+
+## list stores
+* lein run -l
+
+## rescan store by name
+* lein run -s storename -r
+
+## rescan all stores
+* lein run -R
+
+## show basic stats
+* lein run -i
+
+## show data in gui
+* lein run -g
--- src/ic/core.clj
+++ src/ic/core.clj
@@ -1,6 +1,5 @@
 (ns ic.core
-  (:use 
-        [ic.config]
+  (:use [ic.config]
         [ic.db]
         [ic.ui]
         [ic.tools]
@@ -13,38 +12,7 @@
 (info "ic start " (msec))
 
 (defn -main
-  "int-check in clojure
-   so far a basic first example:
-
-    - set algorithm
-      lein run -a md5
-      lein run -a sha-1
-      lein run -a sha-256
-      lein run -a sha-512
-
-    - set interval for next scan in seconds
-      lein run -c 30   ;;(rescan after 30 seconds)
-      lein run -c 2592000  ;;(rescan after 1 month)
-
-    - add new store
-      lein run -s storename -p /path/to/store -n
-      lein run -s otherstore -p /path/to/other/store -n
-
-    - list stores
-      lein run -l
-
-    - rescan store by name
-      lein run -s storename -r
-
-    - rescan all stores
-      lein run -R
-
-    - show basic stats
-      lein run -i
-
-    - show data in gui
-      lein run -g
-  "
+  "run ic"
   [& args]
   (let [[options args banner] (arguments args)]
     (with-connection db
